b323c0df2c8f6762d83f4bed922e5950445321ed
JeanCarlos Jimenez <jeancarlosjimenezc25@gmail.com>
2025-08-10 17:32:33 -0500

    Changes Archives - Fixe

--- src/App.vue
+++ src/App.vue
@@ -4,58 +4,59 @@
 <template>
   <header>
     <code>By_JCT_@2025.bin</code>
-    <h1>GNU-LINUX-OF-JCT.sh</h1>
+    <h1>GNU-LINUX-JCT.sh</h1>
   </header>
   <br>
   <nav>
-    <a href="http://">START</a>
-    <a href="http://">HISTORY</a>
-    <a href="http://">BLOG</a>
-    <a href="http://">IMAGES</a>
-    <a href="http://">ABOUT</a>
+    <a href="#start">START</a>
+    <a href="#history">HISTORY</a>
+    <a href="#blog">BLOG</a>
+    <a href="#images">IMAGES</a>
+    <a href="#about">ABOUT</a>
   </nav>
   <main>
     <hr>
-    <h2>./START.sh</h2>
+    <h2 id="start">./HISTORY.sh</h2>
     <hr>
-    <p><b>E</b>xplora el poder del código. Domina el sistema. En las profundidades de la terminal, donde
-    las líneas <br> de comando cobran vida y el software libre rompe cadenas, nace este sitio
-  Somos más que un sitio <br> somos una comunidad de hackers, sysadmins y entusiastas del código abierto
-   que respiran <br> GNU/Linux, seguridad informática, scripting y de todos.
-  </p>
+    <p><b id="b">E</b>xplora el poder del código. Domina el sistema. En las profundidades de la terminal, donde
+      las líneas <br> de comando cobran vida y el software libre rompe cadenas, nace este sitio
+      Somos más que un sitio <br> somos una comunidad de hackers, sysadmins y entusiastas del código abierto
+      que respiran <br> GNU/Linux, seguridad informática, scripting y de todos.
+    </p>
 
     <hr>
-        <h2>./HISTORY.sh</h2>
+    <h2 id="history">./HISTORY.sh</h2>
     <hr>
     <p>
-      <b>E</b>n los años 80, mientras las corporaciones encerraban el software tras licencias y códigos
-       ocultos, <br> un hacker llamado Richard Stallman declaró guerra al monopolio. Así nació GNU: un sistema 
-       <br> operativo 100% libre, creado por y para los usuarios.
+      <b id="b">E</b>n los años 80, mientras las corporaciones encerraban el software tras licencias y códigos
+      ocultos, <br> un hacker llamado Richard Stallman declaró guerra al monopolio. Así nació GNU: un sistema
+      <br> operativo 100% libre, creado por y para los usuarios.
     </p>
-    <span>Pero faltaba una pieza clave…</span>
+    <p><span>Pero faltaba una pieza clave…</span></p>
     <p>
-      <b>E</b>n 1991, un estudiante finlandés llamado Linus Torvalds liberó el kernel Linux, y la
+      <b id="b">E</b>n 1991, un estudiante finlandés llamado Linus Torvalds liberó el kernel Linux, y la
       comunidad hacker <br> lo fusionó con GNU. Nació GNU/Linux, un sistema indestructible, modificable
-       y libre, donde tú tienes <br> el control.
+      y libre, donde tú tienes <br> el control.
     </p>
-    <p><span>(¿Listo para tomar el control? El link está esperando… �)</span><br><br><a id="p-link" href="http://">VER-MAS.url</a></p>
+    <p><span>(¿Listo para tomar el control? El link está esperando… �)</span><br><br><a id="p-link" href="./components/history.vue">👉
+        Ejecutar Aqui</a></p>
     <hr>
-        <h2>./BLOG.sh</h2>
+    <h2 id="blog">./BLOG.sh</h2>
     <hr>
     <p><b>⚠️ ALERTA: Este blog no es apto para usuarios de Windows.</b> <br>
       Aquí no hay wizards, ni next > next > finish. <br>
-Solo líneas de comando, kernels personalizados y la verdad sin filtros.
-<br>
-<br>
-<b>Si alguna vez:</b>
-<br>
-- Soñaste con romper las reglas del software privativo.
-<br>
-- Sentiste que systemd es el demonio (o lo amaste, no juzgamos).
-<br>
-- Quisiste automatizar tu vida con un script de 3 líneas.
-<br>
-…este es tu nuevo hogar.
+      Solo líneas de comando, kernels personalizados y la verdad sin filtros.
+      <br>
+      <br>
+      <b>Si alguna vez:</b>
+      <br>
+      - Soñaste con romper las reglas del software privativo.
+      <br>
+      - Sentiste que systemd es el demonio (o lo amaste, no juzgamos).
+      <br>
+      - Quisiste automatizar tu vida con un script de 3 líneas.
+      <br>
+      …este es tu nuevo hogar.
     </p>
     <div>
       <b>📌 ¿Aceptas el desafío?</b>
@@ -63,39 +64,45 @@ Solo líneas de comando, kernels personalizados y la verdad sin filtros.
       <p><a id="p-link" href="http://">👉 Entra al Blog</a></p>
     </div>
     <hr>
-        <h2>./IMAGES.sh</h2>
+    <h2 id="images">./IMAGES.sh</h2>
     <hr>
     <div>
-      Desde terminales glitch hasta escritorios que parecen cyberpunk dreams… Linux no solo es código: es
-      <br> arte, personalización y caos controlado. Aquí, un vistazo a distros, custom WMs y setups que harán <br> llorar a tu Windows…
-     <p><strong>(Desliza → para el viaje visual…)</strong></p>
+      <p>
+        <b id="b">D</b>esde terminales glitch hasta escritorios que parecen cyberpunk dreams… Linux no solo es código:
+        es
+        <br> arte, personalización y caos controlado. Aquí, un vistazo a distros, custom WMs y setups que harán <br>
+        llorar a tu Windows…
+      </p>
+      <p><strong>(Desliza → para el viaje visual…)</strong></p>
     </div>
     <div>
-       <img src="./assets/images/image-0.jpeg" />
-       <img src="./assets/images/image-1.jpeg" />
-       <br>
-       <img src="./assets/images/image-2.jpeg" />
-       <p><span>¿Quieres ver más? </span><a id="p-link" href="http://">👉 👉 Click Aqui</a></p>
-    </div>
-    <hr>
-    <h2>./ABOUT.sh</h2>
-    <hr>
-    <div><b>¿</b>Alguna vez te cansaste de que tu SO te trate como un usuario… y no como un hacker?
+      <img src="./assets/images/image-0.jpeg" />
+      <img src="./assets/images/image-1.jpeg" />
       <br>
-      Este nació de una terminal abierta a las 3 AM, entre líneas de Bash y el sonido de un teclado
-      <br> mecánico. No es solo un blog o una galería de Linux… es un manifiesto.
-    <p><span>"Linux es difícil", dijo nadie después de instalar Arch correctamente</span></p>
+      <img src="./assets/images/image-2.jpeg" />
+      <p><span>¿Quieres ver más? </span><a id="p-link" href="http://">👉 Haz Click Aqui</a></p>
+    </div>
+    <hr>
+    <h2 id="about">./ABOUT.sh</h2>
+    <hr>
+    <div>
+      <p><b id="b">¿</b>Alguna vez te cansaste de que tu SO te trate como un usuario… y no como un hacker?
+        <br>
+        Este nació de una terminal abierta a las 3 AM, entre líneas de Bash y el sonido de un teclado
+        <br> mecánico. No es solo un blog o una galería de Linux… es un manifiesto.
+      </p>
+      <p><span>"Linux es difícil", dijo nadie después de instalar Arch correctamente</span></p>
     </div>
     <hr>
     <footer>
-      <h2>./FOOTER.sh</h2>
-      <p>Para todos los que creen que sudo rm -rf / es una filosofía de vida.</p>
-      <p><strong>make install</strong> - <span>exit  # ¿O prefieres CTRL+D?</span></p>
-      <p><span>🚀 2024 - GNU-LINUX-OF-JCT.sh | APACHE II o nada</span></p>
+      <h2 id="footer">./FOOTER.sh</h2>
+      <hr>
+      <p><b id="b">P</b>ara todos los que creen que sudo rm -rf / es una filosofía de vida.</p>
+      <p><strong>make install</strong> - <span>exit # ¿O prefieres CTRL+D?</span></p>
+      <p><span>🚀 2025 - GNU-LINUX-OF-JCT.sh | Apache 2.0 o nada</span></p>
       <hr>
     </footer>
-</main>
+  </main>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
